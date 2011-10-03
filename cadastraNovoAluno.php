9c84d593d598bf5b8f63319770ebddc990de1f01
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2011-10-03 14:59:21 -0300

    Custom Fields example, tested and ok.

--- cadastraNovoAluno.php
+++ cadastraNovoAluno.php
@@ -18,35 +18,44 @@ $url = "$host$install$service";
 
 $token = "99b30439d03a76ec2383601c3174cb5d";
 
-$soapAction = "moodle_user_create_users";
+$soapAction = "sistemaaula_user_create_users"; // somente esta função está portada.
 
 
 $user1 = new stdClass();
-$user1->username = 'usuarioexemplo7';	//String 
+$user1->username = 'usuarioexemplo11';	//String 
 $user1->password = 'Senha_1235r';		// 
-$user1->firstname = 'Primeiro2';		//
+$user1->firstname = 'Primeiro11';		//
 $user1->lastname = 'Ultimo';			//
-$user1->email = 'exemplo7@carlosdelfino.eti.br'; // String
+$user1->email = 'exemplo11@carlosdelfino.eti.br'; // String, obrigatoriamente um e-mail
+// $user1->phone1 = '(31)98387171';		// Esta propriedade eu teste inserir, mas não é aceita
+										// apenas as documentadas são passadas.  
 $user1->auth = 'manual';				// String, metodo de autenticação, 
 										// usar sempre "manual", 
 										// podemos criar um metodo de 
 										// autenticação padrão para o SistemaAula.
-$user1->idnumber = '7305z0z0.a.as222';	// String, até 100 Caracteres, codigo unico que identifica o usuário no processo de integração.
+$user1->idnumber = '11305z0z0.a.as222';	// String, até 100 Caracteres, codigo unico que identifica o usuário no processo de integração.
 $user1->lang = 'pt_br';					// String, Observe que está fora do padrão 
 $user1->theme = 'standard';				// String, padrão para "starndard"
 $user1->timezone = '-3';				// String, usar sempre -3, ou "America/Brazil"
 $user1->mailformat = 1;					// Int, usar 0 para formato plano, ou 1 para formato HTML
-$user1->description = 'Sou o cara  migrado do Sistema Aula para aqui e não onde estou.';
+$user1->description = 'Sou o cara 11 migrado do Sistema Aula usando nova função criada no webservice do sistema aula, para aqui e não onde estou.';
 										// Descrição do usuário.
-//$user1->city = 'Belo Horizonte';
+$user1->city = 'Belo Horizonte';
 $user1->country = 'BR';					// String, país com duas letras, em maiúsculas.
 
-$preferencename1 = array('type' => 'turma', 'value' => 'T-451');
-$preferencename2 = array('type' => 'unidade', 'value' => 'U-4');
+/*
+ * Não consigo ainda com sucesso com o uso dos campos personalizados.
+ * 
+ * Este para funcionarem devem ser usados 
+ */
+$customFields1 = array('type' => 'turma', 'value' => 'T-451');
+$customFields2 = array('type' => 'unidade', 'value' => 'U-4');
+$customFields3 = array('type' => 'codigo', 'value' => '10');
 
-$user1->preferences = array(
-	$preferencename1,
-	$preferencename2
+$user1->customfields = array(
+	$customFields1,
+	$customFields2,
+	$customFields3
 );
 
 $params = array($user1);
