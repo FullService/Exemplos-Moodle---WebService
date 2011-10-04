7553ce5647e75183f280383a3df9f4b05268850c
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2011-10-04 15:54:51 -0300

    Primeiro exemplo criação de curso usando WebService

---
+++ cadastraNovoCurso.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Teste de WebService Moodle</title>
+</head>
+<body>
+
+
+<?php
+
+$host    = "http://192.168.0.38/";
+$install = "moodlerooms/";
+$service = "webservice/soap/server.php";
+
+$url = "$host$install$service";
+
+$token = "99b30439d03a76ec2383601c3174cb5d";
+
+$soapAction = "sistemaaula_course_create_courses"; 
+
+$course1 = new stdClass();
+$course1->fullname = "Nome Completo do Curso";	// string, Nome Completo do Curso
+$course1->shortname = "ncc-1";					// string, Nome Curto 
+$course1->categoryid  = "1";					// int, id da categoria
+												// deve ser conhecido o id conforme já cadastrado no moodle 
+$course1->idnumber  = "axo.23d.0x";				// string, Opcional, Id universal do curso
+$course1->summary  = "Um breve descrição do curso";
+												// string, Opcional, summary
+/*
+$course1->summaryformat  = "";	// int  Padrão para "0" //the summary text Moodle format
+$course1->format  = "";			// string  Padrão para "weeks" //course format: weeks, topics, social, site,..
+$course1->showgrades  = "";		// int  Padrão para "1" //1 if grades are shown, otherwise 0
+$course1->newsitems  = "";		// int  Padrão para "5" //number of recent items appearing on the course page
+$course1->startdate  = "";		// int  Opcional //timestamp when the course start
+$course1->numsections  = "";	// int  Padrão para "10" //number of weeks/topics
+$course1->maxbytes  = "";		// int  Padrão para "8388608" //largest size of file that can be uploaded into the course
+$course1->showreports  = "";	// int  Padrão para "0" //are activity report shown (yes = 1, no =0)
+$course1->visible  = "";		// int  Opcional //1: available to student, 0:not available
+$course1->hiddensections  = "";	// int  Padrão para "0" //How the hidden sections in the course are displayed to students
+$course1->groupmode  = "";		// int  Padrão para "0" //no group, separate, visible
+$course1->groupmodeforce  = "";	// int  Padrão para "0" //1: yes, 0: no
+$course1->defaultgroupingid  = "";	// int  Padrão para "0" //default grouping id
+$course1->enablecompletion  = "";	// int  Opcional //Enabled, control via completion and activity settings. Disabled,
+									//not shown in activity settings.
+$course1->completionstartonenrol  = "";	// int  Opcional //1: begin tracking a student's progress in course completion after
+										// course enrolment. 0: does not
+$course1->completionnotify  = "";		// int  Opcional //1: yes 0: no
+$course1->lang  = "";					// string  Opcional //forced course language
+$course1->forcetheme  = "";				// string  Opcional //name of the force theme
+
+*/
+$params = array($course1);
+
+$serverurl = "$url?wstoken=$token&wsdl=1";
+
+$client = new SoapClient($serverurl);
+try {
+	$resp = $client->__soapCall($soapAction, array($params));
+} catch (Exception $e) {
+	print_r($e);
+}
+print_r($resp);
